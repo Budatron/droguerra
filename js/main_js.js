bfa5cee7913eda5cc49330158a6cf110ba3e747e
Budatron <braincandyweb@gmail.com>
2019-05-08 00:02:30 -0500

    close

--- js/main_js.js
+++ js/main_js.js
@@ -959,12 +959,16 @@
     });
 
     $(".close-app").on('click', function() {
-        if (navigator.app) {
-            navigator.app.exitApp();
-        } else if (navigator.device) {
-            navigator.device.exitApp();
+        if (typeof cordova !== 'undefined') {
+            if (navigator.app) {
+                navigator.app.exitApp();
+            }
+            else if (navigator.device) {
+                navigator.device.exitApp();
+            }
         } else {
             window.close();
+           
         }
     });
 
